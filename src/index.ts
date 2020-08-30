cced4b6f4c2c6172921309ef76d317765423de63
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2020-08-30 21:18:14 +0200

    Changed version number to align with GitHub releases

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { User } from "telegraf/typings/telegram-types";
 import { PlayerStats } from "./stats";
 import { ButtonKeyBoardHelper } from "./button-keyboard-helper";
 
-const version = '1.0.1';
+const version = '1.0.0';
 const confPath = process.argv[2] || './conf';
 const conf: Conf = JSON.parse(fs.readFileSync(confPath + '/conf.json', { encoding: 'UTF-8' }));
 const bot = new Telegraf.default(conf.token);
